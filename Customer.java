b283cba74a50f6a7d0252e7be2131fb1479eaa01
NandiniGondaliya <74456852+NandiniGondaliya@users.noreply.github.com>
2023-02-10 17:14:12 +0530

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,29 @@
+package DAY2;
+
+public class Customer {
+    private String name;
+    private String address;
+    private String mobile;
+
+    Customer(String name,String address,String mobile)
+    {
+        this.name=name;
+        this.address=address;
+        this.mobile=mobile;
+    }
+    public void display()
+    {
+       
+    // for(String myStr: res) 
+    // {
+    //     System.out.println(myStr);
+    // }
+    System.out.println("Name:"+name);
+    System.out.println("Address:"+address);
+    System.out.println("Phone:"+mobile);
+    }
+    
+
+    
+}
+
